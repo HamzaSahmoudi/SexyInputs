e879d52737ceb14f50ef3b8df19d848c74ff334a
HamzaSahmoudi <hamza.sahmoudi@student.ehb.be>
2024-10-23 16:40:08 +0200

    SexyInputs

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-import Cleave from "./../SexyInputs/node_modules/cleave.js/dist/cleave-esm.min.js";
+import Cleave from "./node_modules/cleave.js/dist/cleave-esm.min.js";
 
-var cleaveName = new Cleave("#name", {
-	prefix: "STUDENT-",
+let cleaveName = new Cleave("#name", {
+	prefix: "STUDENT",
 	delimiter: "-",
-	blocks: [7, 3, 3, 4],
+	blocks: [7, 99],
 	uppercase: true,
 });
 
-var cleaveDOB = new Cleave("#DOB", {
+let cleaveDOB = new Cleave("#DOB", {
 	date: true,
 	datePattern: ["d", "m", "Y"],
 });
 
-var cleaveRRN = new Cleave("#RRN", {
+let cleaveRRN = new Cleave("#RRN", {
 	delimiters: [".", ".", "-", "."],
 	blocks: [2, 2, 2, 3, 2],
 	numericOnly: true,
 });
 
-var cleaveAge = new Cleave("#age", {
+let cleaveAge = new Cleave("#age", {
 	numeral: true,
 	numeralPositiveOnly: true,
 });
 
-var cleavePhone = new Cleave("#phone", {
+let cleavePhone = new Cleave("#phone", {
 	phone: true,
-	phoneRegionCode: "BE +32",
+	phoneRegionCode: "BE",
 });
 
 console.log("Hello from main.js");

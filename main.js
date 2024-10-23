225c6ff1a0d0d6611202ddcdd43095855295941d
HamzaSahmoudi <hamza.sahmoudi@student.ehb.be>
2024-10-23 15:19:08 +0200

    exercises session 4

---
+++ main.js
@@ -0,0 +1,31 @@
+import Cleave from "./node_modules/cleave.js/dist/cleave-esm.min.js";
+
+var cleaveName = new Cleave("#name", {
+	prefix: "STUDENT-",
+	delimiter: "-",
+	blocks: [7, 3, 3, 4],
+	uppercase: true,
+});
+
+var cleaveDOB = new Cleave("#DOB", {
+	date: true,
+	datePattern: ["d", "m", "Y"],
+});
+
+var cleaveRRN = new Cleave("#RRN", {
+	delimiters: [".", ".", "-", "."],
+	blocks: [2, 2, 2, 3, 2],
+	numericOnly: true,
+});
+
+var cleaveAge = new Cleave("#age", {
+	numeral: true,
+	numeralPositiveOnly: true,
+});
+
+var cleavePhone = new Cleave("#phone", {
+	phone: true,
+	phoneRegionCode: "BE +32",
+});
+
+console.log("Hello from main.js");
